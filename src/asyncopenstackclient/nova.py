c8b03d5cd7b6d2f8bcc9af20f0056e79e3743456
Patryk Gałczyński <galczynski.patryk@gmail.com>
2018-04-26 11:21:32 +0200

    server metadata partial support (#15)

--- src/asyncopenstackclient/nova.py
+++ src/asyncopenstackclient/nova.py
@@ -3,7 +3,7 @@ from .client import Client
 
 class NovaClient(Client):
     def __init__(self, session=None, api_url=None):
-        super().__init__('nova', ['flavors', 'servers'], session, api_url)
+        super().__init__('nova', ['flavors', 'servers', 'metadata'], session, api_url)
 
     async def init_api(self):
         await super().init_api()
@@ -13,3 +13,11 @@ class NovaClient(Client):
         self.api.servers.add_action("force_delete")
         self.api.servers.add_action("get")
         self.api.flavors.actions["list"] = {"method": "GET", "url": "flavors/detail"}
+        self.api.metadata.actions['get'] = {"method": "GET", "url": "servers/{}/metadata"}
+        self.api.metadata.actions['set'] = {"method": "POST", "url": "servers/{}/metadata"}
+        self.api.metadata.actions['get_item'] = {"method": "GET", "url": "servers/{}/metadata/{}"}
+        self.api.metadata.actions['set_item'] = {"method": "PUT", "url": "servers/{}/metadata/{}"}
+        self.api.metadata.add_action("set_item")
+        self.api.metadata.add_action("get_item")
+        self.api.metadata.add_action("get")
+        self.api.metadata.add_action("set")
